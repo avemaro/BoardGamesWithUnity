5161b35c57bda52d2d1ea4617987436342651674
yamasaki <yamasakikota@gmail.com>
2020-03-20 22:07:34 +0900

    モジュール BoardGames の初期チェックイン

---
+++ Assets/Tests/EditerTests/ReverseTests.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class ReverseTests {
+        [Test]
+        public void TestBoardHasInitialized() {
+            var board = new Board();
+            Assert.AreEqual(board.GetColor(Cell.d5), PieceColor.black);
+            Assert.AreEqual(board.GetColor(Cell.e4), PieceColor.black);
+            Assert.AreEqual(board.GetColor(Cell.d4), PieceColor.white);
+            Assert.AreEqual(board.GetColor(Cell.e5), PieceColor.white);
+        }
+    }
+}
