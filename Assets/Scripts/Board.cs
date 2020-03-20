9f46cf346c42736c8c9b494903cbe750850fd03f
yamasaki <yamasakikota@gmail.com>
2020-03-20 22:26:07 +0900

    Test1-2

---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board
+{
+    public PieceColor GetColor(Cell? cell) {
+        return PieceColor.black;
+    }
+}
