b577ac95d5bd4d298f26ec087b0ca3eaed758ff5
yamasaki <yamasakikota@gmail.com>
2020-03-20 22:32:07 +0900

    Test1-3

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Board
 {
     public PieceColor GetColor(Cell? cell) {
+        if (cell == Cell.d4) return PieceColor.white;
+        if (cell == Cell.e5) return PieceColor.white;
         return PieceColor.black;
     }
 }
