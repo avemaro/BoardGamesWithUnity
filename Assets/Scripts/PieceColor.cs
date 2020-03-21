7cc71bc7dd822e2624180b802b4d8be222756007
yamasaki <yamasakikota@gmail.com>
2020-03-21 10:51:07 +0900

    Test4-REFACTORING-2

--- Assets/Scripts/PieceColor.cs
+++ Assets/Scripts/PieceColor.cs
@@ -5,3 +5,13 @@ using UnityEngine;
 public enum PieceColor{
     black, white, none
 }
+
+public static class PieceColorExtend {
+    public static PieceColor ReversedColor(this PieceColor pieceColor) {
+        switch (pieceColor) {
+            case PieceColor.black: return PieceColor.white;
+            case PieceColor.white: return PieceColor.black;
+        }
+        return PieceColor.none;
+    }
+}
